b621ec8c177ac4b5fccb0a45300c8edb6f46e55d
SuperLsz <2455191288@qq.com>
2019-01-31 17:29:16 +0800

    实体类

---
+++ src/main/java/com/example/dao/UserDO.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+import java.io.Serializable;
+
+public class UserDO implements Serializable {
+
+	/**
+	 * @Fields serialVersionUID : 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
